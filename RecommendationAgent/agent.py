e6ae9de21447a6219ba2327477123720b2ee9f86
Moutaz Muhammad <85679864+moutazmuhammad@users.noreply.github.com>
2025-09-10 16:51:16 +0300

    Update agent.py

--- RecommendationAgent/agent.py
+++ RecommendationAgent/agent.py
@@ -3,6 +3,7 @@ from google.adk.agents import LlmAgent
 from google.adk.tools.agent_tool import AgentTool
 
 from .subagents.parallel_usage_recommender import usage_recommandation_agent
+from .subagents.parallel_remaining_recommender import lead_user_usage_remaining_agent, update_pointes_agent
 
 MODEL = "gemini-2.5-flash"
 APP_NAME = "RecommendationAgent"
@@ -16,5 +17,7 @@ root_agent = LlmAgent(
     output_key="seminal_paper",
     tools=[
         AgentTool(agent=usage_recommandation_agent),
+        AgentTool(agent=lead_user_usage_remaining_agent),
+        AgentTool(agent=update_pointes_agent),
     ],
 )
